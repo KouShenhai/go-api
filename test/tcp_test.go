15f9ba66d2e65be8ee644624e0c1b2484f715def
koushenhai <2413176044@qq.com>
2024-08-30 18:32:53 +0800

    feat(): 提交

--- test/tcp_test.go
+++ test/tcp_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"bytes"
 	"encoding/hex"
 	"fmt"
 	"go-api/util"
@@ -8,7 +9,17 @@ import (
 )
 
 func Test1(t *testing.T) {
-	fmt.Println(hex.EncodeToString(toReadSingle(0x34, 1, 1, 3, 4, 5)))
+	// a0a0a0a00001210100c60007001c0001000100a40000000300000000000000000000000000000000001c00010001008a0000000100000000000000000000000000000000001c0001000100880000000300000000000000000000000000000000001c0001000100880000000400000000000000000000000000000000001c0001000100240000000000000000000000000000000000000000001c0001000100250000000000000000000000000000000000000000001c0001000100a40000000300000000000000000000000000000000
+	array := toBuildReadArray(
+		toReadSingle(0xA4, 3, 0, 0, 0, 0),
+		toReadSingle(0x8A, 1, 0, 0, 0, 0),
+		toReadSingle(0x88, 3, 0, 0, 0, 0),
+		toReadSingle(0x88, 4, 0, 0, 0, 0),
+		toReadSingle(0x24, 0, 0, 0, 0, 0),
+		toReadSingle(0x25, 0, 0, 0, 0, 0),
+		toReadSingle(0xA4, 3, 0, 0, 0, 0),
+	)
+	fmt.Println(hex.EncodeToString(array))
 }
 
 func toReadSingle(a, b, c, d, e, f int32) []byte {
@@ -23,4 +34,17 @@ func toReadSingle(a, b, c, d, e, f int32) []byte {
 	copy(buffer[22:], util.TransByte(e))
 	copy(buffer[26:], util.TransByte(f))
 	return buffer
+}
+
+func toBuildReadArray(bs ...[]byte) []byte {
+	var buffer bytes.Buffer
+	buffer.Write([]byte{0xa0, 0xa0, 0xa0, 0xa0, 0x00, 0x01, 0x21, 0x01, 0x00, 0x1e})
+	buffer.Write(util.TransByte(int32(len(bs))))
+	for _, b := range bs {
+		buffer.Write(b)
+	}
+	result := buffer.Bytes()
+	length := util.TransByte(int32(len(result) - 10))
+	copy(result[8:], length)
+	return result
 }
